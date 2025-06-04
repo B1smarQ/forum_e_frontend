4f47c8d0a01539a4e7674a03125f53c7ada5e3f7
B1smarQ <bgh050021@gmail.com>
2025-06-04 14:07:08 +0400

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import './App.css'
 import Home from "./components/Home"
-import NotFound from "./components/NotFoundPage/NotFound.tsx";
+import NotFound from "./components/NotFoundPage/NotFound.jsx";
 
 function App() {
 
